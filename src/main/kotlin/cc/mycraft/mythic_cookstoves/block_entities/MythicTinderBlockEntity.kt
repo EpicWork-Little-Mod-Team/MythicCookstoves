79e99630f9da2ba2b7cd1a1c8d22c483293b0a44
Sunbread <sunbread233@gmail.com>
2022-07-14 02:17:50 +0800

    make lightning target player

--- src/main/kotlin/cc/mycraft/mythic_cookstoves/block_entities/MythicTinderBlockEntity.kt
+++ src/main/kotlin/cc/mycraft/mythic_cookstoves/block_entities/MythicTinderBlockEntity.kt
@@ -2,12 +2,18 @@ package cc.mycraft.mythic_cookstoves.block_entities
 
 import net.minecraft.core.BlockPos
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket
+import net.minecraft.util.Mth
+import net.minecraft.world.entity.EntitySelector
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.levelgen.Heightmap
 import net.minecraft.world.phys.Vec3
+import java.lang.Math.pow
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.pow
 
 class MythicTinderBlockEntity(pWorldPosition: BlockPos, pBlockState: BlockState) :
     BlockEntity(ModBlockEntities.MYTHIC_TINDER, pWorldPosition, pBlockState) {
@@ -17,13 +23,23 @@ class MythicTinderBlockEntity(pWorldPosition: BlockPos, pBlockState: BlockState)
 
     companion object {
         fun tick(pLevel: Level, pPos: BlockPos, pState: BlockState, pBlockEntity: BlockEntity) {
-            if (
-                pPos.y == pLevel.getHeight(Heightmap.Types.WORLD_SURFACE, pPos.x, pPos.z) - 1 &&
-                pLevel.random.nextInt(400) == 0
-            ) {
-                val lightning = EntityType.LIGHTNING_BOLT.create(pLevel)
-                lightning?.moveTo(Vec3.atBottomCenterOf(pPos))
-                lightning?.let(pLevel::addFreshEntity)
+            if (pPos.y == pLevel.getHeight(Heightmap.Types.WORLD_SURFACE, pPos.x, pPos.z) - 1) {
+                val blockPos = Vec3.atBottomCenterOf(pPos)
+                val target = pLevel.getNearestPlayer(
+                    pPos.x + 0.5,
+                    pPos.y + 0.5,
+                    pPos.z + 0.5,
+                    8.0,
+                    true
+                )
+                val targetPos = if (target != null) Vec3(target.x, target.y, target.z) else blockPos
+                val distance = if (target != null) min(8.0, targetPos.distanceTo(blockPos)) else 8.0
+                val threshold = 2.0.pow((8 - distance) / 2) / 400
+                if (pLevel.random.nextDouble() < threshold) {
+                    val lightning = EntityType.LIGHTNING_BOLT.create(pLevel)
+                    lightning?.moveTo(targetPos)
+                    lightning?.let(pLevel::addFreshEntity)
+                }
             }
         }
     }
