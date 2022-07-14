7cd370f0b9f6ddb437936e1fe338b51f65223ab1
Sunbread <sunbread233@gmail.com>
2022-07-14 17:07:05 +0800

    refactor: clean empty class body

--- src/main/kotlin/cc/mycraft/mythic_cookstoves/blocks/pot/SaucepanBlock.kt
+++ src/main/kotlin/cc/mycraft/mythic_cookstoves/blocks/pot/SaucepanBlock.kt
@@ -3,6 +3,4 @@ package cc.mycraft.mythic_cookstoves.blocks.pot
 import net.minecraft.world.level.material.Material
 import net.minecraft.world.level.material.MaterialColor
 
-class SaucepanBlock : AbstractPotBlock(Properties.of(Material.HEAVY_METAL, MaterialColor.METAL).noOcclusion()) {
-
-}
+class SaucepanBlock : AbstractPotBlock(Properties.of(Material.HEAVY_METAL, MaterialColor.METAL).noOcclusion())
